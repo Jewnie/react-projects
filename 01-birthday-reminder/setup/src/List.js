3438cb228791518b0cefbf6b7542a339c2b70fe0
Jewnie <john_loore@hotmail.com>
2023-12-28 16:44:21 +0100

    .

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from "./data"
 
 const List = () => {
+
+  const [people, setPeople] = useState(data)
+  const handleClear = () => {
+    if (people.length < 1) {
+      setPeople(data)
+      return;
+    }
+    setPeople([])
+  }
+  const handleRemove = (id) => {
+    const newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <img style={{ width: "50px", height: "50px", objectFit: "cover", borderRadius: "50%" }} src={person.image} />
+            <h3>{person.name}</h3>
+            <h4>{person.age}</h4>
+            <button onClick={handleRemove(person.id)}>Remove</button>
+          </div>
+        )
+      })}
+      {people.length > 0 ? <button onClick={handleClear}>Clear List</button> : <button onClick={handleClear}>Reset</button>}
+
     </>
   );
 };
